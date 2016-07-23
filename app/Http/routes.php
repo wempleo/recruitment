6e275af5522a9a4176b3b24d5f77ab93c55c0355
wilson empleo <wilson empleo>
2016-07-24 02:57:29 +0800

    Added Laravel Auth

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
